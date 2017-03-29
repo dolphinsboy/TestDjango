a4a48634ef6cb658822e879f33f2238963842edf
dolphinsboy <sguo421@gmail.com>
2017-03-29 17:48:33 +0800

    自动测试视图的功能

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,7 +10,6 @@ from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 
 from django.views import generic
-from django.views.decorators.http import require_http_methods
 
 # def index(request):
 #     question_list = Question.objects.all()
@@ -43,7 +42,6 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
-@require_http_methods(["POST"])
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
 
