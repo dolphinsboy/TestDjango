42ea6695b46395d5fbe80948de6f5e6d7ee83593
dolphinsboy <sguo421@gmail.com>
2017-03-29 11:24:11 +0800

    添加使用命名空间的URL

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 # from django.template import loader,RequestContext
 from .models import Question
 from django.shortcuts import render
-from django.http import Http404
-
+# from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 def index(request):
     question_list = Question.objects.all()
@@ -14,13 +14,15 @@ def index(request):
     # context = RequestContext(request, {'question_list':question_list})
     # return HttpResponse(template.render(context))
     context = {'question_list': question_list}
-    return render(request,'polls/index.html', context)
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-        context = {'question': question}
-    except Question.DoesNotExist:
-        raise Http404("Question does not exits")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    #     context = {'question': question}
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exits")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
     return render(request, 'polls/detail.html', context)
