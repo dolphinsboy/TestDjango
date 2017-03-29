58039d5ac7f5caf5accbb150801427aa46ea9b87
dolphinsboy <sguo421@gmail.com>
2017-03-29 10:44:15 +0800

    完成detail的处理

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.template import loader,RequestContext
+# from django.http import HttpResponse
+# from django.template import loader,RequestContext
 from .models import Question
+from django.shortcuts import render
+from django.http import Http404
 
 
 def index(request):
     question_list = Question.objects.all()
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {'question_list':question_list})
-    return HttpResponse(template.render(context))
+    # template = loader.get_template('polls/index.html')
+    # context = RequestContext(request, {'question_list':question_list})
+    # return HttpResponse(template.render(context))
+    context = {'question_list': question_list}
+    return render(request,'polls/index.html', context)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+        context = {'question': question}
+    except Question.DoesNotExist:
+        raise Http404("Question does not exits")
+    return render(request, 'polls/detail.html', context)
